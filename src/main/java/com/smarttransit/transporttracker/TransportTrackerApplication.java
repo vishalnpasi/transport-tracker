16017806ae6e5e53fde3849a698740c51e410382
Vishal Pasi <vishalnpasi@gmail.com>
2025-06-24 19:07:27 +0530

    project initialized

---
+++ src/main/java/com/smarttransit/transporttracker/TransportTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.smarttransit.transporttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarttransit/transporttracker/TransportTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttransit.transporttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
